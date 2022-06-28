e3c2f3882010f94eacd9a53fce158ca4c8fe6c9a
zjp <jiping_zhou@foxmail.com>
2022-06-28 14:28:06 +0800

    translate vim to lua for last location jump

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,5 +1,5 @@
 local set = vim.opt
-local fn  = vim.fn
+local api = vim.api
 
 -- show existing tab with 4 spaces width
 set.tabstop = 4
@@ -16,15 +16,32 @@ set.splitbelow = true
 
 set.timeoutlen = 500
 
+set.fillchars:append 'diff:╱'
+
 -- colorscheme
 vim.cmd(':source ' .. vim.fn.stdpath 'config' .. '/lua/colorscheme.vim')
 
 -- 如果没有这个设置，每次打开文件时光标都将定位在第一行。
 -- 而加入了这个设置以后，你就可以恢复到上次关闭文件时光标所在的位置了。
 -- https://github.com/wsdjeg/vim-galore-zh_cn
-vim.cmd [[
-autocmd BufReadPost *
-    \ if line("'\"") > 1 && line("'\"") <= line("$") |
-    \   exe "normal! g`\"" |
-    \ endif
-]]
+-- vim.cmd [[
+-- autocmd BufReadPost *
+--     \ if line("'\"") > 1 && line("'\"") <= line("$") |
+--     \   exe "normal! g`\"" |
+--     \ endif
+-- ]]
+
+-- src: https://www.reddit.com/r/neovim/comments/tqeh9m/comment/i2hbc4p
+local group = api.nvim_create_augroup("jump_last_position", { clear = true })
+api.nvim_create_autocmd(
+  "BufReadPost",
+  {
+    group = group,
+    callback = function()
+      local row, col = unpack(api.nvim_buf_get_mark(0, "\""))
+      if { row, col } ~= { 0, 0 } then
+        api.nvim_win_set_cursor(0, { row, 0 })
+      end
+    end,
+  }
+)
