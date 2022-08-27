d4bcc0222f873cf2efd1ba484bcad1b18503b2af
zjp <jiping_zhou@foxmail.com>
2022-08-27 19:28:13 +0800

    luasnip(md): refactor githash

--- luasnippets/markdown/githash.lua
+++ luasnippets/markdown/githash.lua
@@ -1,3 +1,4 @@
+-- execute bash cmd
 local function shell(cmd)
   local f = io.popen(cmd .. ' 2>/dev/null', 'r') -- ignore errors
   if not f then return '' end
@@ -12,33 +13,12 @@ local function shell(cmd)
 end
 
 -- last hash of commit on current branch
--- print(os.execute 'git log --format="%h" -n 1')
-local githash = s("githash",
-  f(function() return shell 'git branch --show-current' .. '#' .. shell 'git log --format="%h" -n 1' end, {}))
--- last hash of commit on current branch
-local githash_master = s("githash_master",
-  f(function()
-    local hash = shell 'git log --format="%h" -n 1 master'
-    if hash == '' then
-      return ''
-    else
-      return 'master#' .. hash
-    end
-  end, {}))
--- last hash of commit on current branch
-local githash_main = s("githash_main",
-  f(function()
-    local hash = shell 'git log --format="%h" -n 1 main'
-    if hash == '' then
-      return ''
-    else
-      return 'main#' .. hash
-    end
-  end, {}))
+local current_branch_hash = s("current_branch_hash", f(function()
+  return shell 'git branch --show-current' .. '#' .. shell 'git log --format="%h" -n 1'
+end))
 
-local hash_branch = f(function(args)
-  print(vim.inspect(args))
-  local branch = args[1][1]
+-- helper for latest commit generation
+local hash = function(branch)
   if branch == '' then return '' end
   local hash = shell('git log --format="%h" -n 1 ' .. branch)
   if hash == '' then
@@ -46,45 +26,40 @@ local hash_branch = f(function(args)
   else
     return '#' .. hash
   end
-end, { 1 })
-local master_failed = s("master_failed", {
-  i(1, "master"), hash_branch
-})
-local master_ok = s("master_ok", {
-  i(1, "master"), f(function(args)
-    print(vim.inspect(args))
-    local branch = args[1][1]
-    if branch == '' then return '' end
-    local hash = shell('git log --format="%h" -n 1 ' .. branch)
-    if hash == '' then
-      return ''
-    else
-      return '#' .. hash
-    end
-  end, { 1 })
-})
+end
+-- <!-- master#18d2877 --->
+local master = s("master", { f(function() return '<!-- master' .. hash('master') .. ' --->' end), t { "", "" } })
+-- master#18d2877
+local master_hash = s("master_hash", f(function() local b = "master" return b .. hash(b) end))
+-- main#18d2877
+local main_hash = s("main_hash", f(function() local b = "main" return b .. hash(b) end))
 
-local branch_dyn = d(1, function()
+-- main#18d2877 or master#18d2877 or current_branch#18d2877
+local hashtag = s("hashtag", f(function()
+  local main = main_hash:get_docstring()[1]:sub(1, -3)
+  if main ~= "main" then return main end
+  local m = master_hash:get_docstring()[1]:sub(1, -3)
+  if m ~= "master" then return m end
+  return current_branch_hash:get_docstring()[1]:sub(1, -3)
+end))
+
+-- search for branches
+local branches = function()
   local output = shell 'git branch --format "%(refname:short)"'
   local choices = {}
   for branch in output:gmatch("[^ ]+") do
     table.insert(choices, t(branch))
   end
-  return sn(nil, c(1, choices))
-end, {})
-local branch_failed = s("branch_failed", branch_dyn)
-local branch_ok = s("branch_ok", d(1, function()
-  local output = shell 'git branch --format "%(refname:short)"'
-  local choices = {}
-  for branch in output:gmatch("[^ ]+") do
-    table.insert(choices, t(branch))
-  end
-  return sn(nil, c(1, choices))
-end, {}))
-
-local branch_hash = s("branch_hash", { branch_dyn, hash_branch })
+  return choices
+end
+-- select a branch
+local branch = s("branch", d(1, function() return sn(nil, c(1, branches())) end, {}))
+-- select a branch with hash
+local branch_hash = s("branch_hash", {
+  d(1, function() return sn(nil, c(1, branches())) end),
+  f(function(args) return hash(args[1][1]) end, 1)
+})
 
 return {
-  githash, githash_master, githash_main, master_ok, branch_ok, branch_hash,
-  master_failed, branch_failed,
+  current_branch_hash, master, master_hash, main_hash, branch, branch_hash, hashtag,
 }
