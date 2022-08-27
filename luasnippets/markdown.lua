3a23da8720d319bf1f4c7d7b3f5807b62bf21007
zjp <jiping_zhou@foxmail.com>
2022-08-27 10:58:58 +0800

    luasnip(md): branch and hash choices

--- luasnippets/markdown.lua
+++ luasnippets/markdown.lua
@@ -51,18 +51,32 @@ local githash_main = s("githash_main",
       return 'main#' .. hash
     end
   end, {}))
+
+local githash_branch_fn = f(function(args)
+  local branch = args[1][1]
+  if branch == '' then return '' end
+  local hash = shell('git log --format="%h" -n 1 ' .. branch)
+  if hash == '' then
+    return ''
+  else
+    return '#' .. hash
+  end
+end, { 1 })
 local githash_branch = s("githash_branch", {
-  i(1),
-  f(function(args)
-    local branch = args[1][1]
-    if branch == '' then return '' end
-    local hash = shell('git log --format="%h" -n 1 ' .. branch)
-    if hash == '' then
-      return ''
-    else
-      return '#' .. hash
-    end
-  end, { 1 })
+  i(1), githash_branch_fn
+})
+
+local branch_dyn = d(1, function()
+  local output  = shell 'git branch --format "%(refname:short)"'
+  local choices = {}
+  for branch in output:gmatch("[^ ]+") do
+    table.insert(choices, t(branch))
+  end
+  return sn(nil, c(1, choices))
+end, {})
+local branch = s("branch", branch_dyn)
+local branch_hash = s("branch_hash", {
+  branch_dyn, githash_branch_fn
 })
 
 local img_sized = '<img src="${1}" alt="${2}" width="${3}"/>'
@@ -71,5 +85,5 @@ return {
   parse("footnote", footnote),
   rust_ignore,
   parse("img_sized", img_sized),
-  githash, githash_master, githash_main, githash_branch,
+  githash, githash_master, githash_main, githash_branch, branch, branch_hash,
 }
