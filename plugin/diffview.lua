878d87357b3dedad86132859a65b557c76610dcb
zjp <jiping_zhou@foxmail.com>
2022-09-13 13:15:36 +0800

    update(diffview): set default_args

--- plugin/diffview.lua
+++ plugin/diffview.lua
@@ -1,4 +1,8 @@
 require 'diffview'.setup {
   use_icons = false,
   enhanced_diff_hl = true,
+  default_args = {
+    DiffviewOpen = { "--untracked-files=no", },
+    DiffviewFileHistory = { "--base=LOCAL" },
+  },
 }
