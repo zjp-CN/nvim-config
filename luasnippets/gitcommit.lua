17182068bf805b222978f0608e972f424284fca3
zjp <jiping_zhou@foxmail.com>
2022-09-30 00:52:44 +0800

    luasnip(gitcommit): rm NeogitCommitMessage; add update_ci
    NeogitCommitMessage uses the gitcommit's snippets

--- luasnippets/gitcommit.lua
+++ luasnippets/gitcommit.lua
@@ -1,9 +1,11 @@
 local translation = "translation(${1:path}): ${2:title}"
 local luasnip_ = "luasnip(${1:filetype}): ${2:title}"
 local update = "update(${1:something}): ${2:title}"
+local update_ci = "update(ci): ${2:title}"
 
 return {
   parse("translation", translation),
   parse("luasnip", luasnip_),
   parse("update", update),
+  parse("update_ci", update_ci),
 }
