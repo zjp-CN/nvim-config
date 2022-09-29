33e9ff2bc4df20609d188af6bbdca9a6568dada6
zjp <jiping_zhou@foxmail.com>
2022-09-29 16:50:46 +0800

    update(telescope): keymap actions
    * enable cycle_history
    * selection toggle/drop all
    * delete_buffer

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -1,18 +1,34 @@
 local tele = require 'telescope'
+local action = require 'telescope.actions'
 local keymap = require 'keymap'
 local bind = keymap.bind
+
+-- :h telescope.actions
+local mappings = {
+  i = {
+    -- 上翻/下翻历史搜索记录：所有功能共享历史搜索记录
+    ["<C-Down>"] = action.cycle_history_next,
+    ["<C-Up>"] = action.cycle_history_prev,
+  },
+  n = {
+    ["t"] = action.toggle_all, -- 反选所有
+    ["T"] = action.drop_all, -- 取消所有
+    ["d"] = action.delete_buffer,
+  }
+}
 
 tele.setup {
   defaults = {
     layout_strategy = 'vertical',
     layout_config = { vertical = { width = 0.95, height = 0.95, prompt_position = 'top' } },
+    mappings = mappings,
   },
   extensions = {
     ['ui-select'] = {
       require 'telescope.themes'.get_dropdown {},
       file_browser = { theme = 'ivy', hijack_netrw = true, },
     }
-  }
+  },
 }
 tele.load_extension 'ui-select'
 tele.load_extension 'file_browser'
@@ -24,6 +40,7 @@ bind('n', ',f', ':Telescope find_files<CR>')
 bind('n', ',F', ':Telescope oldfiles<CR>')
 bind('n', ',h', ':Telescope highlights<CR>')
 bind('n', ',k', ':Telescope keymaps<CR>')
+bind('n', ',b', ':Telescope buffers<CR>')
 
 -- LSP only keymap
 keymap.autocmd('LSPTelescopeKeybindings',
