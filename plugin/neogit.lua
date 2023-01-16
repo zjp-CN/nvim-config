db599bd3b16776ff8dfb96ec1d21cc67066916f1
zjp <jiping_zhou@foxmail.com>
2023-01-16 13:38:12 +0800

    update(neogit): hide timeout auto_show_console

--- plugin/neogit.lua
+++ plugin/neogit.lua
@@ -1,5 +1,7 @@
 require 'neogit'.setup {
-  integrations = { diffview = true, }
+  integrations = { diffview = true, },
+  auto_show_console = false,
+  console_timeout = 10000,
 }
 
 local bind = require 'keymap'.bind
