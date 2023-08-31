995adeffc4df283670c179464c8d863c82363ac1
zjp <jiping_zhou@foxmail.com>
2023-08-31 14:09:32 +0800

    keymap: exchange default leader/localleader set by lazyvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- exchange default leader/localleader set by lazyvim
+vim.g.mapleader = [[\]]
+vim.g.localleader = [[<space>]]
