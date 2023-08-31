96ba201766c5bbf2a30bc55df9ccb8446ef524b7
zjp <jiping_zhou@foxmail.com>
2023-08-31 23:35:13 +0800

    ui: redo lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,3 +1,14 @@
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed,
+    }
+  end
+end
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -18,6 +29,33 @@ return {
       -- top_down = false, -- the position can only be top/bottom for now
       render = "compact",
       stages = "static",
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      -- options = { icons_enabled = false, theme = "nord" },
+      sections = {
+        lualine_b = {
+          "branch",
+          { "diff", source = diff_source },
+        },
+        lualine_c = {
+          "diagnostics",
+          { "filename", path = 1 },
+        },
+        lualine_x = {
+          "encoding",
+          "filetype",
+          {
+            "fileformat",
+            icons_enabled = true,
+            symbols = { unix = "LF", dos = "CRLF", mac = "CR" },
+          },
+        },
+        lualine_y = {},
+        lualine_z = { "vim.fn.line('.') .. '/' .. vim.fn.line('$') .. ':' .. vim.fn.col('.')" },
+      },
     },
   },
 }
