73afd61fbba6e50e87b51294db336fd24cda5c8a
zjp <jiping_zhou@foxmail.com>
2023-08-31 22:57:43 +0800

    lazy: don't notify on change; default lazy for my custom plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,10 +18,11 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
   },
+  change_detection = { notify = false },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
+    lazy = true,
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
