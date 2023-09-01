22fbc3a598f21497c1d52d4dcb54a706baa2d433
zjp <jiping_zhou@foxmail.com>
2023-09-01 23:11:31 +0800

    hi: float bg, diagnostics error, comment

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -10,3 +10,21 @@ opt.listchars:append("trail:⋅")
 -- don't yank contents to system clipborad,
 -- when system clipborad needs yank, use `+` register (i.e. `"+y`)
 opt.clipboard = ""
+
+-- highlights
+vim.cmd([[
+  " error font color in diagnostic float window
+  " error icon color on left sidebar
+  hi! DiagnosticError guifg=#e86f94
+  " diagnostic float window
+  " hover doc float window
+  hi! NormalFloat guibg=#100642
+
+  " comments in most languages
+  hi! Comment guifg=#00A000
+  " but not for git
+  hi! gitcommitComment guifg=grey80
+  hi! gitignoreComment guifg=grey80
+  hi! gitrebaseComment guifg=grey80
+  hi! gitrebaseMergeComment guifg=grey80
+]])
