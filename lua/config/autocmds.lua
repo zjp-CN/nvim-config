90a9236e064c59cea77742817032c36ce2586f99
zjp <jiping_zhou@foxmail.com>
2023-09-01 19:55:27 +0800

    opt: don't yank to system clipborad, use "+y instead

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,4 +2,11 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.opt.listchars:append("trail:⋅")
+local opt = vim.opt
+
+-- trailing spaces are shown as ⋅
+opt.listchars:append("trail:⋅")
+
+-- don't yank contents to system clipborad,
+-- when system clipborad needs yank, use `+` register (i.e. `"+y`)
+opt.clipboard = ""
