840c1048b7000ba2281638f7a9fae95b9ba9d74a
zjp <jiping_zhou@foxmail.com>
2023-09-02 21:17:10 +0800

    edit: add vim-table-mode
    Note how to create a whichkey group based on keymaps from the plugins.

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -15,4 +15,22 @@ return {
     ft = "qf",
     opts = {},
   },
+  {
+    "dhruvasagar/vim-table-mode",
+    cmd = "TableModeToggle",
+    keys = "<leader>tm",
+    config = function()
+      -- regiter key group info for whichkey
+      require("which-key").register({
+        t = {
+          -- optional group name
+          name = "table mode",
+          -- describe without creating keymaps
+          d = "(table mode) delete row/col",
+          i = "(table mode) insert row/col",
+          f = "(table mode) formula",
+        },
+      }, { prefix = "<leader>" })
+    end,
+  },
 }
