706d15cbd1838f328cb35ab918c95b45ca87c73f
zjp <jiping_zhou@foxmail.com>
2023-09-04 23:59:29 +0800

    edit: read all buffers as a source for nvim-cmp
    Note thers seems no better way to

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -33,4 +33,34 @@ return {
       }, { prefix = "<leader>" })
     end,
   },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      for i, item in ipairs(opts.sources) do
+        for key, value in pairs(item) do
+          if key == "name" and value == "buffer" then
+            item["option"] = {
+              get_bufnrs = function()
+                local bufs = {}
+                -- all buffers (maybe deleted buffers like help page)
+                for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+                  if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_line_count(buf) > 0 then
+                    table.insert(bufs, buf)
+                  end
+                end
+                -- buffers in current window/tab
+                -- for _, win in ipairs(vim.api.nvim_list_wins()) do
+                --   table.insert(bufs, vim.api.nvim_win_get_buf(win))
+                -- end
+                return bufs
+              end,
+            }
+            goto continue
+          end
+        end
+      end
+      ::continue::
+      return opts
+    end,
+  },
 }
