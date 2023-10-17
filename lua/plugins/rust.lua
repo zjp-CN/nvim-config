6ea3f7758575d1f89f62782ccc487be3f915c8fc
zjp <jiping_zhou@foxmail.com>
2023-10-17 16:56:26 +0800

    Rust: kepmap F4~F6 for Cargo.toml via crates.nvim

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -5,4 +5,12 @@ return {
       { "<F2>", "<cmd>RustHoverActions<cr>", desc = "RustHoverActions" },
     },
   },
+  {
+    "Saecki/crates.nvim",
+    keys = {
+      { "<F4>", "<cmd>lua require('crates').show_features_popup()<cr>", desc = "crates: show_features_popup" },
+      { "<F5>", "<cmd>lua require('crates').show_popup()<cr>", desc = "crates: show_popup" },
+      { "<F6>", "<cmd>lua require('crates').show_dependencies_popup()<cr>", desc = "crates: show_dependencies_popup" },
+    },
+  },
 }
