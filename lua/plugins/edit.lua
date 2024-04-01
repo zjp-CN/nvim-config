039d7c1b64dfef5d7b66a4b771644681cebfe735
zjp <jiping_zhou@foxmail.com>
2024-04-01 09:20:44 +0800

    edit: make nvim-cmp-lsp-rs work on nvim-cmp

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -1,18 +1,6 @@
 -- default to use listed buffers as a source for nvim-cmp
 vim.g.cmp_get_bufnrs = "buflisted"
 
-local function entry_filter(e, c)
-  return require("cmp_lsp_rs").filter_out.rust_entry_filter(e, c)
-end
-
-local function inscope_inherent_import(e1, e2)
-  return require("cmp_lsp_rs").comparators.inscope_inherent_import(e1, e2)
-end
-
-local function sort_by_label_but_underscore_last(e1, e2)
-  return require("cmp_lsp_rs").comparators.sort_by_label_but_underscore_last(e1, e2)
-end
-
 local function sql_formatter_config()
   local json = "/lua/plugins/sql_formatter.json"
   local path = (jit.os == "Linux") and json or json:gsub("/", "\\")
@@ -96,66 +84,78 @@ return {
   },
   {
     "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zjp-CN/nvim-cmp-lsp-rs",
+        -- dir = "/rust/github/nvim-cmp-lsp-rs",
+        -- dependencies = { "hrsh7th/nvim-cmp" },
+        -- opts = {},
+        --@param opts cmp_lsp_rs.Opts
+        -- config = function(_, opts)
+        --   require("cmp_lsp_rs").setup(opts)
+        -- end,
+        -- event = { "InsertEnter" },
+        -- main = "cmp_lsp_rs",
+      },
+    },
     keys = {
       { "<leader>b0", "<cmd>let g:cmp_get_bufnrs='current_buf'<cr>", desc = "(nvim-cmp) cmp_get_bufnrs='current_buf'" },
       { "<leader>b1", "<cmd>let g:cmp_get_bufnrs='current_tab'<cr>", desc = "(nvim-cmp) cmp_get_bufnrs='current_tab'" },
       { "<leader>b2", "<cmd>let g:cmp_get_bufnrs='buflisted'<cr>", desc = "(nvim-cmp) cmp_get_bufnrs='buflisted'" },
       { "<leader>ba", "<cmd>let g:cmp_get_bufnrs='current_buf'<cr>", desc = "(nvim-cmp) cmp_get_bufnrs='all'" },
+      -- { "<leader>bc", "<cmd>lua require'cmp_lsp_rs'.combo()<cr>", desc = "(nvim-cmp) switch comparators" },
     },
-    dependencies = {
-      { dir = "/rust/github/nvim-cmp-lsp-rs", opts = {} },
-    },
-    ---@type cmp.ConfigSchema
-    opts = {
-      ---@type cmp.SourceConfig[]
-      sources = {
-        {
-          name = "nvim_lsp",
-          entry_filter = entry_filter,
-        },
-      },
-      sorting = {
-        priority_weight = 2,
-        comparators = {
-          inscope_inherent_import,
-          sort_by_label_but_underscore_last,
-        },
-      },
-    },
-    ----param opts cmp.ConfigSchema
-    -- opts = function(_, opts)
-    --   local cmp = require("cmp")
-    --   -- local compare = require("cmp").config.compare
-    --
-    --   -- disable auto select: always select first candidate instead
-    --   opts.preselect = cmp.PreselectMode.None
-    --
-    --   opts.view = vim.tbl_deep_extend("keep", opts.view or {}, {
-    --     docs = { auto_open = false },
-    --   })
-    --
-    --   local cmp_rs = require("cmp_lsp_rs")
-    --   local comparators = cmp_rs.comparators
-    --
-    --   opts.sorting.comparators = {
-    --     -- compare.kind,
-    --     -- comparators.inherent_import_inscope,
-    --     -- comparators.inscope_inherent,
-    --     comparators.inscope_inherent_import,
-    --     comparators.sort_by_label_but_underscore_last,
-    --     -- compare.recently_used,
-    --     -- compare.sort_text,
-    --   }
-    --
-    --   for _, source in ipairs(opts.sources) do
-    --     if source.name == "buffer" then
-    --       source.option = vim.tbl_deep_extend("keep", { get_bufnrs = get_bufnrs }, source.option or {})
-    --     end
-    --
-    --     cmp_rs.filter_out.entry_filter(source)
-    --   end
-    --   return opts
-    -- end,
+    -- ---@type cmp.ConfigSchema
+    -- opts = {
+    --   ---@type cmp.SourceConfig[]
+    --   sources = {
+    --     {
+    --       name = "nvim_lsp",
+    --       entry_filter = entry_filter,
+    --     },
+    --   },
+    --   sorting = {
+    --     priority_weight = 2,
+    --     comparators = {
+    --       inscope_inherent_import,
+    --       sort_by_label_but_underscore_last,
+    --     },
+    --   },
+    -- },
+    --@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      -- local compare = require("cmp").config.compare
+
+      -- disable auto select: always select first candidate instead
+      opts.preselect = cmp.PreselectMode.None
+
+      opts.view = vim.tbl_deep_extend("keep", opts.view or {}, {
+        docs = { auto_open = false },
+      })
+
+      local cmp_rs = require("cmp_lsp_rs")
+      local comparators = cmp_rs.comparators
+
+      opts.sorting.comparators = {
+        -- compare.kind,
+        -- comparators.inherent_import_inscope,
+        -- comparators.inscope_inherent,
+        comparators.inscope_inherent_import,
+        comparators.sort_by_label_but_underscore_last,
+        -- compare.recently_used,
+        -- compare.sort_text,
+      }
+
+      for _, source in ipairs(opts.sources) do
+        if source.name == "buffer" then
+          source.option = vim.tbl_deep_extend("keep", { get_bufnrs = get_bufnrs }, source.option or {})
+        end
+
+        cmp_rs.filter_out.entry_filter(source)
+      end
+      return opts
+    end,
   },
   {
     "stevearc/conform.nvim",
