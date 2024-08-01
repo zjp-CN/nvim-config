1b0293b4029103c0248c9fb68f0855b8abbe5ad6
zjp <jiping_zhou@foxmail.com>
2024-08-01 16:05:25 +0800

    lsp: add border for hover & signatureHelp
    thanks to https://vi.stackexchange.com/questions/39074/user-borders-around-lsp-floating-windows

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,6 +7,20 @@
 --   autostart = false,
 -- })
 
+local _border = "double"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = _border,
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  border = _border,
+})
+
+vim.diagnostic.config({
+  float = { border = _border },
+})
+
 return {
   -- basic config
   {
