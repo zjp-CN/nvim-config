4aab5f058964951fef340a0f74d150183c975747
zjp <jiping_zhou@foxmail.com>
2024-09-23 08:32:10 +0800

    edit: add quicker for quickfix

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -270,4 +270,11 @@ return {
       },
     },
   },
+  {
+    "stevearc/quicker.nvim",
+    event = "FileType qf",
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    opts = {},
+  },
 }
