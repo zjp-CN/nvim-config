7635980cf9b65eb5dcd184bae7857bc084bfd108
zjp <jiping_zhou@foxmail.com>
2024-12-10 15:55:54 +0800

    opt: remove border setup in vim.lsp.handlers which is deprecated

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,14 +8,6 @@ vim.g.localleader = [[<space>]]
 
 local border = "double"
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = border,
-})
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = border,
-})
-
 vim.diagnostic.config({
   float = { border = border },
 })
