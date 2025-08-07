bedda19f3ff9815d42e966152140f178f9d51237
zjp <jiping_zhou@foxmail.com>
2025-08-07 05:25:30 +0000

    rust: status_notify_level = false to not pop up on single file projects

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -2,13 +2,13 @@ return {
   {
     "mrcjkb/rustaceanvim",
     keys = {
-      { "<F2>",           "<cmd>RustLsp hover actions<cr>", ft = "rust", desc = "RustHoverActions" },
-      { "<space><space>", "<cmd>RustLsp flyCheck<cr>",      ft = "rust", desc = "Rust Fly Check" },
+      { "<F2>", "<cmd>RustLsp hover actions<cr>", ft = "rust", desc = "RustHoverActions" },
+      { "<space><space>", "<cmd>RustLsp flyCheck<cr>", ft = "rust", desc = "Rust Fly Check" },
     },
     opts = {
       tools = {
         test_executor = "termopen", -- default to a popup and hinder --nocapture arg
-        enable_nextest = false,     -- default is true
+        enable_nextest = false, -- default is true
         -- float_win_config = { border = "double" },
       },
       server = {
@@ -19,19 +19,21 @@ return {
               memoryLayout = { enable = true, niches = true, size = "both" },
               show = { fields = 50, enumVariants = 50 },
             },
+            -- check = { command = "check" },
             checkOnSave = true,
             rustc = { source = "discover" },
             rustcSource = "discover", -- old settings in RA
           },
         },
+        status_notify_level = false,
       },
     },
   },
   {
     "Saecki/crates.nvim",
     keys = {
-      { "<F4>", "<cmd>lua require('crates').show_features_popup()<cr>",     desc = "crates: show_features_popup" },
-      { "<F5>", "<cmd>lua require('crates').show_popup()<cr>",              desc = "crates: show_popup" },
+      { "<F4>", "<cmd>lua require('crates').show_features_popup()<cr>", desc = "crates: show_features_popup" },
+      { "<F5>", "<cmd>lua require('crates').show_popup()<cr>", desc = "crates: show_popup" },
       { "<F6>", "<cmd>lua require('crates').show_dependencies_popup()<cr>", desc = "crates: show_dependencies_popup" },
     },
   },
