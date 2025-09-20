c9bbc0d7ac150b0139638725e4040b0beb4f2160
zjp <jiping_zhou@foxmail.com>
2025-09-20 15:10:23 +0000

    options: add custom LSP server nrs
    A neovim config version for
    https://github.com/IWANABETHATGUY/tower-lsp-boilerplate

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,3 +15,33 @@ local border = "double"
 -- vim.lsp.buf.hover({ border })
 -- vim.lsp.buf.signature_help({ border })
 vim.diagnostic.config({ float = { border } })
+
+-- **************** Custom LSP config ****************
+
+-- Add custom LSP configuration, especially LSP server location and for what filetypes.
+vim.lsp.config["nrs"] = {
+  -- Command and arguments to start the server.
+  cmd = { "/home/gh-zjp-CN/tmp/tower-lsp-boilerplate/target/debug/nrs-language-server" },
+  -- Filetypes to automatically attach to.
+
+  filetypes = { "nrs" },
+  -- Sets the "workspace" to the directory where any of these files is found.
+
+  -- Files that share a root directory will reuse the LSP server connection.
+  -- Nested lists indicate equal priority, see |vim.lsp.Config|.
+  root_markers = { { ".nrs" }, ".git" },
+
+  -- Specific settings to send to the server. The schema is server-defined.
+  settings = {
+    editor = { semanticHighlighting = { enabled = true } },
+  },
+}
+
+-- Auto set up nrs filetype because neovim doesn't do it for us.
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.nrs" },
+  command = "set filetype=nrs",
+})
+
+-- Make LSP server config into effects.
+vim.lsp.enable("nrs")
