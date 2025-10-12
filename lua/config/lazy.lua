8a1e2e480c8071dc6a0cefb7793299e4c7fc3ec0
zjp <jiping_zhou@foxmail.com>
2025-10-12 08:20:43 +0000

    lsp: enable vue plugins and remove outdated volar settings

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,9 +21,9 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.rust" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.vue" },
+    { import = "lazyvim.plugins.extras.lang.vue" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -51,33 +51,6 @@ return {
               outputPath = "$root/target/$dir_lsp",
             },
           },
-
-          -- ref: https://github.com/LazyVim/LazyVim/pull/6238
-          volar = { -- when LazyVim switches to nvim-lspconfig ≥ v2.2.0 rename this to `vue_ls`
-            on_init = function(client)
-              client.handlers["tsserver/request"] = function(_, result, context)
-                -- find the vtsls client
-                local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
-                if #clients == 0 then
-                  vim.notify("Could not find `vtsls` client, Vue LSP features will be disabled", vim.log.levels.ERROR)
-                  return
-                end
-                local ts_client = clients[1]
-                -- unpack the forwarded request
-                local params = unpack(result)
-                local id, command, payload = unpack(params)
-                -- forward it
-                ts_client:exec_cmd({
-                  title = "vue_request_forward",
-                  command = "typescript.tsserverRequest",
-                  arguments = { command, payload },
-                }, { bufnr = context.bufnr }, function(_, resp)
-                  -- send the tsserver/response back to Vue LSP
-                  client.notify("tsserver/response", { { id, resp.body } })
-                end)
-              end
-            end,
-          },
         },
       }
 
