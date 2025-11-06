07f4a8f54f24af98e10a4e7024c1a7dbf1e5478a
zjp <jiping_zhou@foxmail.com>
2025-11-06 02:25:27 +0000

    options: sync with system clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,7 +8,7 @@ vim.g.localleader = [[<space>]]
 vim.g.lazyvim_picker = "telescope"
 
 vim.opt.conceallevel = 0 -- no hidden symbols especially in markdown
-vim.opt.clipboard = "" -- disable sync with system clipboard
+vim.opt.clipboard = "unnamedplus" -- set this to '' to disable sync with system clipboard
 
 local border = "double"
 
