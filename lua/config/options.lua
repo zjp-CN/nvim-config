1a526985919d230337479e9f0883c9ee26aecc4e
zjp <jiping_zhou@foxmail.com>
2026-04-13 13:11:34 +0000

    options: enable undotree and map <space>u to open it

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,6 +12,10 @@ vim.opt.relativenumber = false
 vim.opt.conceallevel = 0 -- no hidden symbols especially in markdown
 vim.opt.clipboard = "unnamedplus" -- set this to '' to disable sync with system clipboard
 
+-- enable undotree and map the key to open
+vim.cmd("packadd nvim.undotree")
+vim.keymap.set("n", "<space>u", require("undotree").open)
+
 -- Restore jumping to next/previous diff location in diff mode. (TreeSitter overrides them.)
 vim.api.nvim_create_autocmd("OptionSet", {
   pattern = "diff",
